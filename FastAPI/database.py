6db30d8bb17752d0de1488bb5e370065c252da54
Darya Beigi <darya.beigi@ondiem.com>
2026-02-15 22:26:14 -0800

    Converted DB from SQLite to Postgres
    I have updated my selected database to default to using Postgres, as it is
    better for scale and management, especially when working outside of a
    local development environment. I plan to have this work hosted online
    and would not be able to run this project properly on SQLite.

--- FastAPI/database.py
+++ FastAPI/database.py
@@ -1,10 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
 
-URL_DATABASE = 'sqlite:///./finance.db'
+load_dotenv()
 
-engine = create_engine(URL_DATABASE, connect_args={"check_same_thread": False})
+URL_DATABASE = os.getenv('DATABASE_URL', 'sqlite:///./finance.db')
+
+if URL_DATABASE.startswith('postgres://'):
+  URL_DATABASE = URL_DATABASE.replace('postgres://', 'postgresql://', 1)
+
+connect_args = {"check_same_thread": False} if URL_DATABASE.startswith('sqlite') else {}
+
+engine = create_engine(URL_DATABASE, connect_args=connect_args)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- FastAPI/main.py
+++ FastAPI/main.py
@@ -13,7 +13,10 @@ origins = [
 
 app.add_middleware(
   CORSMiddleware,
-  allow_origins=origins
+  allow_origins=origins,
+  allow_credentials = True,
+  allow_methods = ['*'],
+  allow_headers=['*']
 )
 
 class TransactionBase(BaseModel):
