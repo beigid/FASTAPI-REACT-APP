7b5d3a688d7641b698d5bf8582adb5f2a93567e3
Darya Beigi <darya.beigi@ondiem.com>
2026-02-15 22:49:17 -0800

    Added Netlify URL to CORS origins list
    I added the new Netlify URL to my CORS origin list so that my
    production website is able to reach my FASTApi backend without
    running into any blockage.

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -8,7 +8,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 origins = [
-  'http://localhost:3001'
+  'http://localhost:3001',
+  'your-own-finance-app.netlify.app'
 ]
 
 app.add_middleware(
